498eeb8a63bfd77957fcd1c71cbee4e68efada81
jahangirahmadi2003-hash <jahangirahmadi2003@gmail.com>
2026-06-07 11:23:21 +0430

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,5 +1,4 @@
 export default async function handler(req, res) {
-  // فقط درخواست POST از تلگرام قبول می‌کنیم
   if (req.method !== "POST") {
     return res.status(200).send("Bot API is running");
   }
@@ -10,14 +9,12 @@ export default async function handler(req, res) {
     const chatId = update?.message?.chat?.id;
     const text = update?.message?.text;
 
-    // اگر پیام نبود
     if (!chatId || !text) {
       return res.status(200).send("no message");
     }
 
     const TOKEN = process.env.BOT_TOKEN;
 
-    // جواب دادن به تلگرام
     await fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
       method: "POST",
       headers: {
